d7b25059713354347174dbca3129663e960a305e
danCZika <dobos.andras95@gmail.com>
2022-09-25 20:18:35 +0100

    login and logout added

--- tennisLeague/core/urls.py
+++ tennisLeague/core/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rules', views.rules, name='rules'),
     path('register', views.register, name='register'),
+    path('login', views.login_view, name='login'),  
+    path('logout', views.logout_view, name='logout'),
 ]
